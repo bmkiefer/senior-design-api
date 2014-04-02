7d673bf99f44d162eadd96f630ce551607e15435
Brandon Michael Kiefer <brandonmichaelkiefer@wireless-nat-inside.uiowa.edu>
2014-04-01 21:32:07 -0500

    changed create game route

--- app/controllers/api/v1/manage_games_controller.rb
+++ app/controllers/api/v1/manage_games_controller.rb
@@ -22,22 +22,24 @@ class Api::V1::ManageGamesController < ApplicationController
 
    game_date = DateTime.new(params[:game][:year],params[:game][:month],params[:game][:day])
   
-   home_org = Organization.find(params[:game][:home_organization])
+   home_org = Organization.find_by_name(params[:game][:home_organization])
   
-   away_org = Organization.find(params[:game][:away_organization])
+   away_org = Organization.find_by_name(params[:game][:away_organization])
   
-   game = Game.create(:home_score => 0, :away_score => 0, :sport => params[:game][:sport], :home_organization => home_org.name, :away_organization => away_org.name, :date => game_date )
+#   game = Game.create(:home_score => 0, :away_score => 0, :sport => params[:game][:sport], :home_organization => home_org.name, :away_organization => away_org.name, :date => game_date )
    
-   if Team.where(:organization_id => params[:game][:home_organization], :sport => params[:game][:sport]).length == 0
-     Team.create(:organization_id => params[:game][:home_organization], :sport => params[:game][:sport])
+   game = Game.create(:home_score => 0, :away_score => 0, :sport => params[:game][:sport], :home_organization => params[:game][:home_organization], :away_organization => params[:game][:away_organization], :date => game_date )
+
+   if Team.where(:organization_id => home_org.id, :sport => params[:game][:sport]).length == 0
+     Team.create(:organization_id => home_org.id, :sport => params[:game][:sport])
    end
 
-   if Team.where(:organization_id => params[:game][:away_organization], :sport => params[:game][:sport]).length == 0
-     Team.create(:organization_id => params[:game][:away_organization], :sport => params[:game][:sport])
+   if Team.where(:organization_id => away_org.id , :sport => params[:game][:sport]).length == 0
+     Team.create(:organization_id => away_org.id , :sport => params[:game][:sport])
    end
 
-   HomeTeam.create( :team_id => Team.where(:organization_id => params[:game][:home_organization], :sport => params[:game][:sport] ).pluck(:id).first, :game_id => game.id)
-   AwayTeam.create( :team_id => Team.where(:organization_id => params[:game][:away_organization], :sport => params[:game][:sport] ).pluck(:id).first, :game_id => game.id)
+   HomeTeam.create( :team_id => Team.where(:organization_id => home_org.id , :sport => params[:game][:sport] ).pluck(:id).first, :game_id => game.id)
+   AwayTeam.create( :team_id => Team.where(:organization_id => away_org.id , :sport => params[:game][:sport] ).pluck(:id).first, :game_id => game.id)
 
    render :status => 200,
            :json => { :success => true,
